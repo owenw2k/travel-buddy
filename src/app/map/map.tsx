22373b6e054f3d34d01ca3a996beded397044e6a
Owen Wolman <owenw2k@gmail.com>
2024-08-27 10:23:38 -0500

    separate out american and world maps

--- src/app/map/map.tsx
+++ src/app/map/map.tsx
@@ -1,37 +1,12 @@
-import {
-    ComposableMap,
-    Geographies,
-    Geography,
-    ZoomableGroup
-  } from "react-simple-maps";
-
 import { useSelector } from 'react-redux'
+import WorldMap from './worldMap';
+import AmericaMap from './americaMap';
 
 export default function Map() {
-  let world = useSelector((state: any) => state.world)
-  const geoUrl = world ? '/worldMap.json' : '/americaMap.json';
-  
-  return (
-    <div>
-      <ComposableMap projection="geoMercator">
-        <ZoomableGroup center={[0, 0]} zoom={9}>
-          <Geographies geography={geoUrl}>
-            {({ geographies }) =>
-              geographies.map((geo) => (
-                <Geography 
-                  key={geo.rsmKey} 
-                  geography={geo}  
-                  style={{
-                    default: { outline: "none", fill: "#525666" },
-                    hover: { outline: "none", fill: "#6f88e8" },
-                    pressed: { outline: "none", fill: "#02A" },
-                  }}
-                />
-              ))
-            }
-          </Geographies>
-        </ZoomableGroup>
-      </ComposableMap>
-    </div>
-  );
+    let world = useSelector((state: any) => state.world)
+    return(
+        <>
+            {world ? <WorldMap/> : <AmericaMap/>}
+        </>
+    );
 }
