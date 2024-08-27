4c529e77ade13e8ed98c2cce30dfaf01cb32441c
Owen Wolman <owenw2k@gmail.com>
2024-08-27 12:06:26 -0500

    states on america map fill on click

--- src/app/map/americaMap.tsx
+++ src/app/map/americaMap.tsx
@@ -3,43 +3,49 @@ import {
     ZoomableGroup,
     Geographies,
     Geography,
-    Marker,
 } from "react-simple-maps";
+import { SetStateAction, useState } from "react";
 
 export default function AmericaMap() {
+    const [clickedStates, setClickedState] = useState<string[]>([]);
+    const geoUrl = '/americaMap.json';
 
-  const geoUrl = '/americaMap.json';
+    const handleClick = (geo: { properties: { name: any; }; }) => {
+        setClickedState([...clickedStates, geo.properties.name]);
+    }
   
-  return (
-    <div className="map-container">
-        <ComposableMap
-            projection='geoAlbersUsa'
-            projectionConfig={{ scale: 800 }}
-            width={980}
-            height={551}
-            style={{
-                width: "100%",
-                height: "auto",
-            }}
-        >
-             <ZoomableGroup center={[ -97, 40 ]} disablePanning>
-                <Geographies  geography={geoUrl}>  
-                    {({ geographies}) =>
-                        geographies.map((geo) => (
-                            <Geography 
-                            key={geo.rsmKey} 
-                            geography={geo}  
-                            style={{
-                                default: { outline: "none", fill: "#525666" },
-                                hover: { outline: "none", fill: "#6f88e8" },
-                                pressed: { outline: "none", fill: "#02A" },
-                            }}
-                            />
-                        ))
-                    }
-                </ Geographies>
-             </ZoomableGroup>
-        </ComposableMap>
-    </div>
-  );
+    return (
+        <div className="map-container">
+            <ComposableMap
+                projection='geoAlbersUsa'
+                projectionConfig={{ scale: 800 }}
+                width={980}
+                height={551}
+                style={{
+                    width: "100%",
+                    height: "auto",
+                }}
+            >
+                <ZoomableGroup center={[ -97, 40 ]}>
+                    <Geographies  geography={geoUrl}>  
+                        {({ geographies}) =>
+                            geographies.map((geo) => (
+                                <Geography 
+                                    key={geo.rsmKey} 
+                                    geography={geo}  
+                                    style={{
+                                        default: { outline: "none"},
+                                        hover: { outline: "none", fill: "#6f88e8" },
+                                        pressed: { outline: "none"},
+                                    }}
+                                    fill={clickedStates.includes(geo.properties.name) ? "#02A" : "#525666"}
+                                    onClick={() => handleClick(geo)}
+                                />
+                            ))
+                        }
+                    </ Geographies>
+                </ZoomableGroup>
+            </ComposableMap>
+        </div>
+    );
 }
