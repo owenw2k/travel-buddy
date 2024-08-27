77be2e2580eafc881b3835ecbb3e71e8bcc104a5
Owen Wolman <owenw2k@gmail.com>
2024-08-27 10:51:51 -0500

    set up albers projection

--- src/app/map/americaMap.tsx
+++ src/app/map/americaMap.tsx
@@ -1,36 +1,45 @@
 import {
     ComposableMap,
+    ZoomableGroup,
     Geographies,
     Geography,
-    ZoomableGroup
-  } from "react-simple-maps";
-
+    Marker,
+} from "react-simple-maps";
 
 export default function AmericaMap() {
 
   const geoUrl = '/americaMap.json';
   
   return (
-    <div>
-      <ComposableMap projection="geoMercator">
-        <ZoomableGroup center={[0, 0]} zoom={9}>
-          <Geographies geography={geoUrl}>
-            {({ geographies }) =>
-              geographies.map((geo) => (
-                <Geography 
-                  key={geo.rsmKey} 
-                  geography={geo}  
-                  style={{
-                    default: { outline: "none", fill: "#525666" },
-                    hover: { outline: "none", fill: "#6f88e8" },
-                    pressed: { outline: "none", fill: "#02A" },
-                  }}
-                />
-              ))
-            }
-          </Geographies>
-        </ZoomableGroup>
-      </ComposableMap>
+    <div className="map-container">
+        <ComposableMap
+            projection='geoAlbersUsa'
+            projectionConfig={{ scale: 1000 }}
+            width={980}
+            height={551}
+            style={{
+                width: "100%",
+                height: "auto",
+            }}
+        >
+             <ZoomableGroup center={[ -97, 40 ]} disablePanning>
+                <Geographies  geography={geoUrl}>  
+                    {({ geographies}) =>
+                        geographies.map((geo) => (
+                            <Geography 
+                            key={geo.rsmKey} 
+                            geography={geo}  
+                            style={{
+                                default: { outline: "none", fill: "#525666" },
+                                hover: { outline: "none", fill: "#6f88e8" },
+                                pressed: { outline: "none", fill: "#02A" },
+                            }}
+                            />
+                        ))
+                    }
+                </ Geographies>
+             </ZoomableGroup>
+        </ComposableMap>
     </div>
   );
 }
